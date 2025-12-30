d7b8b4f17bac70647e6277e448d4f722efc87bff
Tangent Chang <andy23512@gmail.com>
2025-12-30 21:32:49 +0800

    style: refine generate post code

--- bin/generate-posts.ts
+++ bin/generate-posts.ts
@@ -1,5 +1,5 @@
-import { readFileSync, writeFileSync } from "fs";
 import moment from "moment";
+import { readFileSync, writeFileSync } from "node:fs";
 import { Note } from "../model/hackmd.model";
 import { NoteTableEntry } from "../model/note-table-entry.model";
 import { slugify } from "../util/slugify";
@@ -10,51 +10,50 @@ function replaceNoteUrl(
 ) {
   let result = noteContent;
   Object.entries(urlToFileName).forEach(([url, fileName]) => {
-    result = result.replace(
-      new RegExp(`\\[([^\\]]+)\\]\\(${url}(#[^)]+)?\\)`, "g"),
+    result = result.replaceAll(
+      new RegExp(String.raw`\[([^\]]+)\]\(${url}(#[^)]+)?\)`, "g"),
       `<a href="{% post_path ${fileName} %}$2">$1</a>`
     );
   });
   return result;
 }
 
-(async () => {
-  const rawNotes: Note[] = JSON.parse(
-    readFileSync("./res/hackmd-note-data.json", { encoding: "utf8" })
+const rawNotes: Note[] = JSON.parse(
+  readFileSync("./res/hackmd-note-data.json", { encoding: "utf8" })
+);
+const noteTableData: NoteTableEntry[] = JSON.parse(
+  readFileSync("./res/note-table-data.json", { encoding: "utf8" })
+);
+const notes = rawNotes.filter(
+  (n) =>
+    /^[\w\-,' ()]+$/.exec(n.title) &&
+    n.title !== "Tangent's CharaChorder and Forge Notebook" &&
+    n.title !== "Tangent's CharaChorder and Forge Note List"
+);
+const urlToFileName: Record<string, string> = {};
+for (const note of rawNotes) {
+  const url = note.publishLink.replace("https://hackmd.io", "");
+  const fileName = slugify(note.title);
+  urlToFileName[url] = fileName;
+}
+for (const note of notes) {
+  const url = note.publishLink.replace("https://hackmd.io", "");
+  const noteTableEntry = noteTableData.find((entry) =>
+    entry.enNoteUrl.endsWith(url)
   );
-  const noteTableData: NoteTableEntry[] = JSON.parse(
-    readFileSync("./res/note-table-data.json", { encoding: "utf8" })
-  );
-  const notes = rawNotes.filter(
-    (n) =>
-      n.title.match(/^[\w\-,' ()]+$/) &&
-      n.title !== "Tangent's CharaChorder and Forge Notebook" &&
-      n.title !== "Tangent's CharaChorder and Forge Note List"
-  );
-  const urlToFileName: Record<string, string> = {};
-  for (const note of rawNotes) {
-    const url = note.publishLink.replace("https://hackmd.io", "");
-    const fileName = slugify(note.title);
-    urlToFileName[url] = fileName;
+  if (!noteTableEntry) {
+    console.warn(`No note table entry found for note: ${note.title}`);
+    continue;
   }
-  for (const note of notes) {
-    const url = note.publishLink.replace("https://hackmd.io", "");
-    const noteTableEntry = noteTableData.find((entry) =>
-      entry.enNoteUrl.endsWith(url)
-    );
-    if (!noteTableEntry) {
-      console.warn(`No note table entry found for note: ${note.title}`);
-      continue;
-    }
-    const zhTwNote = rawNotes.find(
-      (n) => n.publishLink === noteTableEntry.zhTwNoteUrl
-    );
-    if (!zhTwNote) {
-      console.warn(`No zh tw note found for note: ${note.title}`);
-      continue;
-    }
-    const fileName = urlToFileName[url];
-    const markdownFileContent = `---
+  const zhTwNote = rawNotes.find(
+    (n) => n.publishLink === noteTableEntry.zhTwNoteUrl
+  );
+  if (!zhTwNote) {
+    console.warn(`No zh tw note found for note: ${note.title}`);
+    continue;
+  }
+  const fileName = urlToFileName[url];
+  const markdownFileContent = `---
 title: ${note.title}
 date: ${moment(note.createdAt).toISOString()}
 updated: ${moment(note.lastChangedAt).toISOString()}
@@ -76,39 +75,27 @@ otherLanguages:
 ---
 ${replaceNoteUrl(
   note.content
-    .replace(/\[TOC\]\n/g, "")
-    .replace(/\n##### /g, "\n###### ")
-    .replace(/\n#### /g, "\n##### ")
-    .replace(/\n### /g, "\n#### ")
-    .replace(/\n## /g, "\n### ")
-    .replace(/\n# /g, "\n## ")
-    .replace(
+    .replaceAll("[TOC]\n", "")
+    .replaceAll("\n##### ", "\n###### ")
+    .replaceAll("\n#### ", "\n##### ")
+    .replaceAll("\n### ", "\n#### ")
+    .replaceAll("\n## ", "\n### ")
+    .replaceAll("\n# ", "\n## ")
+    .replaceAll(
       /:::spoiler (.*)\n([\S\s]*?):::/g,
       '{% collapsecard "$1" %}$2{% endcollapsecard %}'
     )
-    .replace(
+    .replaceAll(
       /:::spoiler\n([\S\s]*?):::/g,
       '{% collapsecard "Show Detail" %}$1{% endcollapsecard %}'
     )
-    .replace(/:::info/g, "{% blockquote %}")
-    .replace(/:::warning/g, "{% blockquote %}")
-    .replace(/:::/g, "{% endblockquote %}")
-    .replace(/\[^\w+\]/g, " $0")
-    /*
-    .replace(
-      /```mermaid\n(---[\S\s]*?---\n)?([\S]+)([^`]+)```/g,
-      mermaidReplacer
-    )
-    */
-    .replace(/:heavy_check_mark:/g, '<div class="check"></div>'),
+    .replaceAll(":::info", "{% blockquote %}")
+    .replaceAll(":::warning", "{% blockquote %}")
+    .replaceAll(":::", "{% endblockquote %}")
+    .replaceAll(/\[^\w+\]/g, " $0")
+    .replaceAll(":heavy_check_mark:", '<div class="check"></div>'),
   urlToFileName
 )}
 `;
-    writeFileSync(`source/_posts/${fileName}.md`, markdownFileContent);
-  }
-})();
-
-function mermaidReplacer(_: string, p1: string, p2: string, p3: string) {
-  p3 = p3.replace(/tickInterval 1day\n/g, "");
-  return `{% mermaid ${p2} %}${p3}{% endmermaid %}`;
+  writeFileSync(`source/_posts/${fileName}.md`, markdownFileContent);
 }
